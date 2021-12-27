3806415c6a8df4d2200409fa5fab06e05fe0aed2
IkoAfianando <ikoafianando123@gmail.com>
2021-12-27 08:34:13 +0700

    Push

---
+++ coba.js
@@ -0,0 +1,33 @@
+// const getUserSync = (id) => {
+//     // let  nama = '';
+//     // if(id === 1) {
+//     //     nama = "Iko";
+//     // }else {
+//     //     nama = "Afianando";
+//     // }
+//     const nama = id === 1 ? "Iko" : "Afianando";
+//     return {id, nama};
+// }
+//
+// const userSatu = getUserSync(1);
+// console.info(userSatu);
+//
+// const userDua = getUserSync(2);
+// console.info(userDua);
+//
+// const hello = "Hello World!";
+// console.info(hello);
+
+
+const user = require("./getUserSync.js");
+
+const userSatu = user(1, (hasil) => {
+    console.log(hasil);
+});
+
+const userDua = user(2, (hasil) => {
+    console.log(hasil);
+});
+
+const hello = "Hello World!";
+console.log(hello);
