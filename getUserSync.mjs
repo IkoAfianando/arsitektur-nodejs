3bd036b95933b283f8e46a6e8e8c3d206d3ae8ed
IkoAfianando <ikoafianando123@gmail.com>
2021-12-27 08:35:01 +0700

    Push

---
+++ getUserSync.mjs
@@ -0,0 +1,9 @@
+const user = (data, callback) => {
+    const time = data === 1 ? 2000 : 3000;
+    setTimeout(() => {
+        const id = data === 1 ? "Iko" : "Afianando";
+        callback({data, id});
+    }, time);
+}
+
+export {user};
